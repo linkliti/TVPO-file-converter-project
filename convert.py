b3065e254ba5f5fb54d03d7761ed605a0d59a74c
mordakin <chumik_2002@mail.ru>
2022-10-12 14:16:32 +0300

    fix PEP:8

--- convert.py
+++ convert.py
@@ -1,93 +1,103 @@
-class unit:
-    def __init__(self, unit_type, name, short_name, SI_factor):
+"""aaaa."""
+
+
+class Unit:
+    """Unit class."""
+
+    def __init__(self, unit_type, name, short_name, si_factor):
+        """__init__ function."""
         self.type = unit_type
         self.name = name
         self.short_name = short_name
-        self.SI_factor = SI_factor
+        self.si_factor = si_factor
 
 # - - - - - - - - - - - - - - - - - -
 # UNIT DEFINITIONS
 # - - - - - - - - - - - - - - - - - -
 
+
 # TIME
-seconds = unit("time", "seconds", "s", 1)
-milliseconds = unit("time", "milliseconds", "ms", 0.001)
-minutes = unit("time", "minutes", "min", 60)
-hours = unit("time", "hours", "h", 3600)
+seconds = Unit("time", "seconds", "s", 1)
+milliseconds = Unit("time", "milliseconds", "ms", 0.001)
+minutes = Unit("time", "minutes", "min", 60)
+hours = Unit("time", "hours", "h", 3600)
 
 # LENGTH
-millimeter = unit("length", "millimeter", "mm", 1/1000)
-centimeter = unit("length", "centimeter", "cm", 1/100)
-meter = unit("length", "meter", "m", 1)
-kilometer = unit("length", "kilometer", "km", 1000)
+millimeter = Unit("length", "millimeter", "mm", 1/1000)
+centimeter = Unit("length", "centimeter", "cm", 1/100)
+meter = Unit("length", "meter", "m", 1)
+kilometer = Unit("length", "kilometer", "km", 1000)
 
 # AREA
-millimeters_squared = unit("area", "millimeters squared", "mm2", 1/1000000)
-centimeters_squared = unit("area", "centimeters squared", "cm2", 1/10000)
-meters_squared = unit("area", "meters squared", "m2", 1)
-kilometers_squared = unit("area", "kilometers squared", "km2", 1000000)
+millimeters_squared = Unit("area", "millimeters squared", "mm2", 1/1000000)
+centimeters_squared = Unit("area", "centimeters squared", "cm2", 1/10000)
+meters_squared = Unit("area", "meters squared", "m2", 1)
+kilometers_squared = Unit("area", "kilometers squared", "km2", 1000000)
 
 # VOLUME
-millimeters_cubed = unit("volume", "millimeters cubed", "mm3", 1/1000000000)
-centimeters_cubed = unit("volume", "centimeters cubed", "cm3", 1/1000000)
-milliliter = unit("volume", "milliliter", "mL", 1/1000000)
-liter = unit("volume", "liter", "L", 1/1000)
-meters_cubed = unit("volume", "meters cubed", "m3", 1)
-kilometers_cubed = unit("volume", "kilometers cubed", "km3", 1000000000)
+millimeters_cubed = Unit("volume", "millimeters cubed", "mm3", 1/1000000000)
+centimeters_cubed = Unit("volume", "centimeters cubed", "cm3", 1/1000000)
+milliliter = Unit("volume", "milliliter", "mL", 1/1000000)
+liter = Unit("volume", "liter", "L", 1/1000)
+meters_cubed = Unit("volume", "meters cubed", "m3", 1)
+kilometers_cubed = Unit("volume", "kilometers cubed", "km3", 1000000000)
 
 # PRESSURE
-pascal = unit("pressure", "pascal", "Pa", 1)
-kilopascal = unit("pressure", "kilopascal", "kPa", 1000)
-megapascal = unit("pressure", "megapascal", "MPa", 1000000)
-gigapascal = unit("pressure", "gigapascal", "GPa", 1000000000)
-bar = unit("pressure", "bar", "bar", 100000)
-atm = unit("pressure", "atmosphere", "atm", 101325)
+pascal = Unit("pressure", "pascal", "Pa", 1)
+kilopascal = Unit("pressure", "kilopascal", "kPa", 1000)
+megapascal = Unit("pressure", "megapascal", "MPa", 1000000)
+gigapascal = Unit("pressure", "gigapascal", "GPa", 1000000000)
+bar = Unit("pressure", "bar", "bar", 100000)
+atm = Unit("pressure", "atmosphere", "atm", 101325)
 
 # ENERGY
-joule = unit("energy", "joule", "J", 1)
-kilojoule = unit("energy", "kilojoule", "kJ", 1000)
-megajoule = unit("energy", "megajoule", "MJ", 1000000)
+joule = Unit("energy", "joule", "J", 1)
+kilojoule = Unit("energy", "kilojoule", "kJ", 1000)
+megajoule = Unit("energy", "megajoule", "MJ", 1000000)
 
-time_units = [seconds, milliseconds, minutes, hours]
-length_units = [millimeter, centimeter, meter, kilometer]
-area_units = [millimeters_squared, centimeters_squared,
+time_UNITS = [seconds, milliseconds, minutes, hours]
+length_UNITS = [millimeter, centimeter, meter, kilometer]
+area_UNITS = [millimeters_squared, centimeters_squared,
               meters_squared, kilometers_squared]
-volume_units = [millimeters_cubed, centimeters_cubed,
+volume_UNITS = [millimeters_cubed, centimeters_cubed,
                 milliliter, liter, meters_cubed, kilometers_cubed]
-pressure_units = [pascal, kilopascal, megapascal, gigapascal, bar, atm]
-energy_units = [joule, kilojoule, megajoule]
+pressure_UNITS = [pascal, kilopascal, megapascal, gigapascal, bar, atm]
+energy_UNITS = [joule, kilojoule, megajoule]
 
-units = [time_units, length_units, area_units, volume_units, pressure_units, energy_units]
+UNITS = [time_UNITS, length_UNITS, area_UNITS,
+         volume_UNITS, pressure_UNITS, energy_UNITS]
 
 
 def convert_unit(value, original_unit, converted_unit):
-    global units
-
-    for unit_list in units:
+    """convert_unit function."""
+    for unit_list in UNITS:
         for j in unit_list:
             if original_unit == j.short_name or original_unit == j.name:
                 original_unit = j
 
-    for unit_list in units:
+    for unit_list in UNITS:
         for j in unit_list:
             if converted_unit == j.short_name or converted_unit == j.name:
                 converted_unit = j
 
     if not original_unit.type == converted_unit.type:
-        print("ERROR: Incompatible unit types for conversion, can't convert " + original_unit.name + " (" +
-              original_unit.short_name + ") to " + converted_unit.name + " (" + converted_unit.short_name + ").")
+        print("ERROR: Incompatible Unit types for conversion,\
+             can't convert " + original_unit.name + " (" +
+              original_unit.short_name + ") to " + converted_unit.name +
+              " (" + converted_unit.short_name + ").")
         return None
 
     if not original_unit.type == "temperature":
-        return value * (original_unit.SI_factor / converted_unit.SI_factor)
+        return value * (original_unit.si_factor / converted_unit.si_factor)
     else:
         pass
 
 
 def main():
-    original_unit = input('Enter original unit: ')
+    """Main's function."""
+    original_unit = input('Enter original Unit: ')
     value = float(input('Enter value: '))
-    converted_unit = input('Enter converted unit: ')
+    converted_unit = input('Enter converted Unit: ')
     print(convert_unit(value, original_unit, converted_unit))
 
 
